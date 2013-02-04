af323ee1597ba139f328d922141c9716871d3731
Girish S <girish.sonawane@gmail.com>
2013-02-04 12:38:40 +0530

    added ./vendor in gemspec

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require
-require "js-logger"
+require "rails-client-logger"
 
 module Dummy
   class Application < Rails::Application
