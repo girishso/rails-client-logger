33619bdd073c1db17a353df1832df3046bb59470
Girish S <girish.sonawane@gmail.com>
2014-09-05 10:35:32 +0530

    integrate ExceptionNotifier

--- app/controllers/rails_client_logger/rails_client_loggers_controller.rb
+++ app/controllers/rails_client_logger/rails_client_loggers_controller.rb
@@ -2,7 +2,11 @@ module RailsClientLogger
   class RailsClientLoggersController < ApplicationController
     def log
       if %w(debug info warn error fatal).include?(params[:level].to_s)
-        Rails.logger.send(params[:level].to_s.to_sym, params[:message])
+        level = params[:level].to_s.to_sym
+        Rails.logger.send(level, params[:message])
+        if defined? ExceptionNotifier
+          ExceptionNotifier.notify_exception(Exception.new(params[:message]), env: request.env) if level == :fatal or level == :error
+        end
         head :ok
       else
         head :bad_request
